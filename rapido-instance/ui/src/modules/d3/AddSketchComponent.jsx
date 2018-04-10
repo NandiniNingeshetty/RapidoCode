1dee2005a7c0df16de7b3854012f4026f06ecb3b
GPravallika <37169124+GPravallika@users.noreply.github.com>
2018-04-10 11:12:30 +0530

    Toggle button in new sketches is done

--- rapido-instance/ui/src/modules/d3/AddSketchComponent.jsx
+++ rapido-instance/ui/src/modules/d3/AddSketchComponent.jsx
@@ -3,24 +3,24 @@ import { browserHistory } from 'react-router'
 import { addEmptySketch, createSketch, exportDesign } from '../utils/TreeActions';
 import ProjectService from '../d3/ProjectServices'
 import AlertContainer from 'react-alert'
-import {showAlert, AlertOptions} from '../utils/AlertActions'
+import { showAlert, AlertOptions } from '../utils/AlertActions'
 import Button from 'mineral-ui/Button';
-import {Link} from "react-router";
+import { Link } from "react-router";
 import TextInput from 'mineral-ui/TextInput';
 import TextArea from 'mineral-ui/TextArea';
 
 var component;
 
 
-export default class extends React.Component{
-  
+export default class extends React.Component {
+
   constructor(props) {
     super(props);
     component = this;
     this.state = {
       projectInfo: {
         projectName: '',
-        projectDesc: ''        
+        projectDesc: ''
       },
       sketchType: true
     };
@@ -32,26 +32,26 @@ export default class extends React.Component{
   }
 
   componentDidMount() {
-   let projectInfo = JSON.parse(sessionStorage.getItem('selectedSketch'));
-   console.log(projectInfo);
-   let teamId = JSON.parse(sessionStorage.getItem('teamId'));
-   let updateMode = sessionStorage.getItem("updateMode")
-    if(teamId) {
+    let projectInfo = JSON.parse(sessionStorage.getItem('selectedSketch'));
+    console.log(projectInfo);
+    let teamId = JSON.parse(sessionStorage.getItem('teamId'));
+    let updateMode = sessionStorage.getItem("updateMode")
+    if (teamId) {
       this.setState({
-          teamId: teamId
+        teamId: teamId
       })
     }
-    if(updateMode == "true") {
-      if(projectInfo){
-      this.setState({
-        projectInfo: {
-          projectName: projectInfo.name,
-          projectDesc: projectInfo.description
-        }
-      
-      })
-    }
-    }else{
+    if (updateMode == "true") {
+      if (projectInfo) {
+        this.setState({
+          projectInfo: {
+            projectName: projectInfo.name,
+            projectDesc: projectInfo.description
+          }
+
+        })
+      }
+    } else {
       this.setState({
         projectInfo: {
           projectName: "",
@@ -59,7 +59,7 @@ export default class extends React.Component{
         }
       })
     }
-   // sessionStorage.setItem('updateMode', false)
+    // sessionStorage.setItem('updateMode', false)
   }
 
   /* Method to select sketch type personal*/
@@ -72,13 +72,13 @@ export default class extends React.Component{
     var personal = document.getElementById("personalDIV");
     var shared = document.getElementById("sharedDIV");
     if (this.state.sketchType) {
-        personal.style.display = "none";
-        shared.style.display = "block";
+      personal.style.display = "none";
+      shared.style.display = "block";
     } else {
       personal.style.display = "block";
       shared.style.display = "none";
     }
-}
+  }
 
   /* Method to select sketch type shared*/
   selectSketchTypeShared() {
@@ -90,13 +90,13 @@ export default class extends React.Component{
     var personal = document.getElementById("personalDIV");
     var shared = document.getElementById("sharedDIV");
     if (this.state.sketchType) {
-        personal.style.display = "none";
-        shared.style.display = "block";
+      personal.style.display = "none";
+      shared.style.display = "block";
     } else {
       personal.style.display = "block";
       shared.style.display = "none";
     }
-}
+  }
 
   /* Method to fetch child upon tree selection */
   fetchChild(parent, fetch, childrenFn) {
@@ -113,18 +113,18 @@ export default class extends React.Component{
 
   /* Method to update Active state for Tree */
   setActiveStatus(activeObj) {
-    component.fetchChild(activeObj, function(d) {
+    component.fetchChild(activeObj, function (d) {
       d.active = false;
       if (d.children) {
         d.children.forEach((item, index) => {
-          item.pId = d.pId + (index+1);
+          item.pId = d.pId + (index + 1);
           item.active = false;
         })
       }
     },
-    function(d) {
-       return d.children && d.children.length > 0 ? d.children : null;
-    });
+      function (d) {
+        return d.children && d.children.length > 0 ? d.children : null;
+      });
   }
 
   /* Method to Export Sketch Data */
@@ -133,9 +133,9 @@ export default class extends React.Component{
   }
 
   /* Method to Show Alert Message */
-  showAlert(message){
+  showAlert(message) {
     showAlert(this, message)
-    setTimeout(function(){
+    setTimeout(function () {
       this.msg.removeAll()
     }.bind(this), 3000);
   }
@@ -144,7 +144,7 @@ export default class extends React.Component{
   handleSubmit(event) {
     event.preventDefault();
     let updateMode = sessionStorage.getItem("updateMode")
-    console.log("editmode",updateMode)
+    console.log("editmode", updateMode)
     // if(updateMode){
     //   var projectInfo = this.state.projectInfo;
     //   component.setActiveStatus(component.state.treedata);
@@ -158,22 +158,22 @@ export default class extends React.Component{
     //       projectDesc:projectInfo.projectDesc
     //     }
     //   })
-      
+
     //   browserHistory.push('/vocabulary');
     // }else 
-      if (this.showFormErrors()) {
-        var projectInfo = this.state.projectInfo;
-        addEmptySketch(this, []);
-        component.setActiveStatus(component.state.treedata);
-        component.state.projectDetails.projectName = projectInfo.projectName;
-        component.state.projectDetails.projectDesc = projectInfo.projectDesc;
-        /* TODO Promise */
-        createSketch(component, [], ProjectService, browserHistory)
+    if (this.showFormErrors()) {
+      var projectInfo = this.state.projectInfo;
+      addEmptySketch(this, []);
+      component.setActiveStatus(component.state.treedata);
+      component.state.projectDetails.projectName = projectInfo.projectName;
+      component.state.projectDetails.projectDesc = projectInfo.projectDesc;
+      /* TODO Promise */
+      createSketch(component, [], ProjectService, browserHistory)
 
-      }
- 
+    }
 
-   // }
+
+    // }
 
   }
 
@@ -181,15 +181,15 @@ export default class extends React.Component{
   /* Method to handle Form Input change */
   handleChange(field, event) {
     event.target.classList.add('active');
-    if(this.state.projectInfo == undefined){
+    if (this.state.projectInfo == undefined) {
       this.state = {
         projectInfo: {
           projectName: '',
-          projectDesc: ''        
+          projectDesc: ''
         }
       }
     }
-    if(event.target.name === 'projectName') {
+    if (event.target.name === 'projectName') {
       this.setState({
         projectInfo: {
           projectName: event.target.value,
@@ -202,7 +202,7 @@ export default class extends React.Component{
         projectInfo: {
           projectName: this.state.projectInfo.projectName,
           projectDesc: event.target.value
-        }       
+        }
       });
     }
   }
@@ -211,17 +211,17 @@ export default class extends React.Component{
   showFormErrors() {
     const inputs = document.getElementById('projectDetailsNode').querySelectorAll('#InputprojectName');
     let isFormValid = true;
-    
+
     inputs.forEach(input => {
       input.classList.add('active');
-      
+
       const isInputValid = this.showInputError(input.name);
-      
+
       if (!isInputValid) {
         isFormValid = false;
       }
     });
-    
+
     return isFormValid;
   }
 
@@ -233,7 +233,7 @@ export default class extends React.Component{
     const isPassword = refName.indexOf('password') !== -1;
     const isProjectName = refName === 'projectName';
 
-    if(isProjectName) {
+    if (isProjectName) {
       if (this.refs.projectName.value.length > 35) {
         this.refs.projectName.setCustomValidity('projectName Field exceeded 35 characters limit');
       } else {
@@ -243,58 +243,60 @@ export default class extends React.Component{
 
     if (!validity.valid) {
       if (validity.valueMissing) {
-        error.textContent = `${label} is a required field`; 
+        error.textContent = `${label} is a required field`;
       } else if (isProjectName && validity.customError) {
         error.textContent = 'projectName Field exceeded 35 characters limit';
       }
       return false;
     }
-    
+
     error.textContent = '';
     return true;
   }
 
-  handleSketchType(event){
-    alert(event.target.value)
-  if(event.target.value === "shared"){
-    document.getElementById("sharedDiv").style.display = "block";
-    document.getElementById("personalDiv").style.display = "none";
-  }
-  else{
-    document.getElementById("sharedDiv").style.display = "none";
-    document.getElementById("personalDiv").style.display = "block";
-  }
+  handleSketchType(event) {
+    this.setState({
+      sketchType: !this.state.sketchType
+    })
+    if (event.target.value === "shared") {
+      document.getElementById("sharedDiv").style.display = "block";
+      document.getElementById("personalDiv").style.display = "none";
+    }
+    else {
+      document.getElementById("sharedDiv").style.display = "none";
+      document.getElementById("personalDiv").style.display = "block";
+    }
   }
 
   /* Render Method */
   render() {
-    return(
+    return (
       <div>
-      <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />      
-      <div className="row sketch-border">
-        <div className="col-md-12 sketch-header">
-          <div className="col-md-2 sketches-text">
-            New Sketch
+        <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
+        <div className="row sketch-border">
+          <div className="col-md-12 sketch-header">
+            <div className="col-md-2 sketches-text">
+              New Sketch
+          </div>
           </div>
         </div>
-      </div>
-      <div className="col-md-8 create-project-section" id="projectDetailsNode">
-        <form className="col-md-8" noValidate onSubmit={this.handleSubmit}>
-          <div className="col-md-12">
-          <label className="Sketch-Name" htmlFor="InputprojectName" id="projectNameLabel">Sketch Type</label>
+        <div className="col-md-8 create-project-section" id="projectDetailsNode">
+          <form className="col-md-8" noValidate onSubmit={this.handleSubmit}>
+            <div className="col-md-12">
+              <label className="Sketch-Name" htmlFor="InputprojectName" id="projectNameLabel">Sketch Type</label>
 
-<div className="switch">
-      <input type="radio" className="switch-input" name="view" value="personal" id="personal" checked={this.state.sketchType} onChange={this.handleSketchType.bind(this)}/>
-      <label htmlFor="personal" className="switch-label">Personal</label>
-      <input type="radio" className="switch-input" name="view" value="shared" id="shared"  checked={!this.state.sketchType} onChange={this.handleSketchType.bind(this)}/>
-      <label htmlFor="shared" className="switch-label">Shared</label>
-      
-    </div>
+              <div className="switch">
+                <input type="radio" className="switch-input" name="view" value="personal" id="personal" checked={this.state.sketchType} onChange={this.handleSketchType.bind(this)} />
+                <label htmlFor="personal" className="switch-label">Personal</label>
+                <input type="radio" className="switch-input" name="view" value="shared" id="shared" checked={!this.state.sketchType} onChange={this.handleSketchType.bind(this)} />
+                <label htmlFor="shared" className="switch-label">Shared</label>
+
+              </div>
             </div>
             <div id="personalDiv" className="col-md-12 next-section">
-              <div className="form-group">                        
+              <div className="form-group">
                 <label className="Sketch-Name" htmlFor="InputprojectName" id="projectNameLabel">Sketch Name</label>
-              <input
+                <input
                   type="text"
                   value={this.state.projectInfo.projectName}
                   className="Rectangle-5 form-control"
@@ -302,9 +304,9 @@ export default class extends React.Component{
                   name="projectName"
                   ref="projectName"
                   onChange={this.handleChange.bind(this, 'projectName')}
-                  required/>                 
+                  required />
 
-                  <div className="error" id="projectNameError" ></div>
+                <div className="error" id="projectNameError" ></div>
               </div>
               <div className="form-group">
                 <label className="Description" htmlFor="InputProjectDesc" id="projectDescLabel">Description</label>
@@ -314,21 +316,27 @@ export default class extends React.Component{
                   className="Rectangle-5-Copy"
                   id="InputProjectDesc"
                   name="projectDesc"
-                 
-                  onChange={this.handleChange.bind(this, 'projectDesc')}
-                  />
-              </div>
-          </div>
-         <div id="sharedDiv" className="col-md-12 next-section displayNone">
-         Shared Div Form
-         </div>
-          <div className="col-md-8 pull-right button-section">
 
-          <Button className="new-sketch-text pull-right"  variant="regular"  onClick={this.handleSubmit} primary>Proceed</Button>
-          <Link to="/sketches"><Button className="pull-right Rectangle-4-Copy-6">Cancel</Button></Link>
-          </div>
-        </form>
-      </div>
+                  onChange={this.handleChange.bind(this, 'projectDesc')}
+                />
+              </div>
+            </div>
+            <div id="sharedDiv" className="col-md-12 next-section displayNone">
+            <div className="form-group">
+                <label className="Sketch-Name" htmlFor="InputprojectName" id="projectNameLabel">Sketch Name</label>
+                <input
+                  type="text"
+                  className="Rectangle-5 form-control" 
+                  required />
+                  </div>
+         </div>
+            <div className="col-md-8 pull-right button-section">
+
+              <Button className="new-sketch-text pull-right" variant="regular" onClick={this.handleSubmit} primary>Proceed</Button>
+              <Link to="/sketches"><Button className="pull-right Rectangle-4-Copy-6">Cancel</Button></Link>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }

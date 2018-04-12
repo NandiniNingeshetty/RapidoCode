d72bc78644b7f39848cb659c3ad8f738385f9d63
TANT-A01\kumom01 <kumom01@ca.com>
2018-04-12 13:10:43 +0530

    Vocabulary show and hide

--- rapido-instance/ui/src/modules/vocabulary/VocabularyEditor.jsx
+++ rapido-instance/ui/src/modules/vocabulary/VocabularyEditor.jsx
@@ -26,7 +26,9 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
     thisComponent = this;
-    this.state = {  
+    this.state = { 
+      closeVocab : true, 
+      openVocab : false,
       portalLoginForm : true,
       portalConnectingSection: false,
       portalConnectionSuccess: false,    
@@ -184,10 +186,27 @@ export default class extends React.Component {
   }, 8000);
 }
 
+  /*Show hide Vocabulary tab*/
+  showVocabulary(e) {
+    e.preventDefault();
+    if(this.state.closeVocab) {
+      this.setState({ closeVocab: false,
+                openVocab : true})
+    }
+    else {
+      this.setState({ closeVocab: true,
+        openVocab : false})
+    }
+  }
+
   /* Render method */
   render() {
     let addOption, loadedComponent;
     let editSketchInfo;
+
+    const VocabularyHideContent = <div className="view-text vocabulary-text vocabulary-closed-panel"><img src="/ui/src/images/bitmap.png" /></div>
+
+    const VocabularyContent = <AddVocabulary selectedSketch={this.state.selectedSketch} />
 
     const myScrollbar = {
       width: "63%",
@@ -422,10 +441,14 @@ export default class extends React.Component {
 <div className="">
 
 <div className="vocabulary-div">
-<AddVocabulary selectedSketch={this.state.selectedSketch} />
+{this.state.closeVocab && VocabularyHideContent}
+<div className="vocabulary-div-panel">
+{/* <AddVocabulary selectedSketch={this.state.selectedSketch} /> */}
+{this.state.openVocab && VocabularyContent}
+</div>
 </div>
 <div className="" id="sidebar">
-<div className="vocabulary-text view-text"><span className="vocabulary-show-hide"><i><img className="" src="/ui/src/images/arrow-left.png" alt="open vocab"/></i></span>SKETCH</div>
+<div className="vocabulary-text view-text"><span className="vocabulary-show-hide"><i className="vocab-arrow" onClick={this.showVocabulary.bind(this)}><img src="/ui/src/images/arrow-left.png" alt="open vocab"/></i></span>SKETCH</div>
 <CRUDSketch getCurrentNodeDetails={this.getCurrentNodeDetails} />
 </div>
 
